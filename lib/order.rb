4ba5eb7cac3df9ccb0e6df38145587b8bc389be8
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-06-11 23:23:30 +0100

    order can place a dish and a quantity in the basket

--- lib/order.rb
+++ lib/order.rb
@@ -3,4 +3,8 @@ class Order
   def initialize
     @basket = {}
   end
+
+  def add_dish(dish, quantity)
+    @basket[dish] = quantity
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,12 +1,16 @@
 require 'order'
 
 describe Order do
-  order = Order.new
+  let(:order) { Order.new }
+  let(:burger) { double :dish, name: 'burger', price: 3 }
+  let(:chips) { double :dish, name: 'chips', price: 1 }
   it 'has an empty basket when created' do
     expect(order.basket).to eq({})
   end
 
-  xit 'can place a dish and a quantity in the basket' do
+  it 'can place a dish and a quantity in the basket' do
+    order.add_dish(burger, 2)
+    expect(order.basket).to eq({ burger => 2 })
   end
 
   xit 'for a specific dish, cannot place a bigger quantity than available' do
